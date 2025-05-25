17124472956d4d23987dd3aa89004d0753a57a54
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2025-05-25 14:54:38 +0300

    UPDATE

---
+++ src/components/widgets/User/HomeAdvantages/index.jsx
@@ -0,0 +1,42 @@
+import clsx from 'clsx'
+import cls from './index.module.scss'
+import { UiContainer } from '@shared/ui/UiContainer'
+import { UiTypography } from '@shared/ui/UiTypography'
+
+const HomeAdvantages = ({ className, ...props }) => {
+	return (
+		<section className={clsx(cls.wrapper, className)} {...props}>
+			<UiContainer className={cls.container} Tag='ul'>
+				<li className={cls.item}>
+					<img src='/images/User/Home/Advantages/premium.svg' alt='premium' />
+					<UiTypography font='JosefinSans' Tag='h2'>
+						Только премиальные материалы
+					</UiTypography>
+				</li>
+				<li className={cls.item}>
+					<img
+						src='/images/User/Home/Advantages/environmentalFriendliness.svg'
+						alt='environmentalFriendliness'
+					/>
+					<UiTypography font='JosefinSans' Tag='h2'>
+						Экологичность
+					</UiTypography>
+				</li>
+				<li className={cls.item}>
+					<img src='/images/User/Home/Advantages/strength.svg' alt='strength' />
+					<UiTypography font='JosefinSans' Tag='h2'>
+						Прочность
+					</UiTypography>
+				</li>
+				<li className={cls.item}>
+					<img src='/images/User/Home/Advantages/reliable.svg' alt='reliable' />
+					<UiTypography font='JosefinSans' Tag='h2'>
+						Надежное сотрудничество
+					</UiTypography>
+				</li>
+			</UiContainer>
+		</section>
+	)
+}
+
+export { HomeAdvantages }
--- src/screens/User/Home/index.jsx
+++ src/screens/User/Home/index.jsx
@@ -2,12 +2,14 @@ import clsx from 'clsx'
 import cls from './index.module.scss'
 import { HomeHero } from '@widgets/User/HomeHero'
 import { HomeAboutFabric } from '@widgets/User/HomeAboutFabric'
+import { HomeAdvantages } from '@widgets/User/HomeAdvantages'
 
 const HomeScreen = ({ className, ...props }) => {
 	return (
 		<main className={clsx(cls.main, className)} {...props}>
 			<HomeHero />
 			<HomeAboutFabric />
+			<HomeAdvantages />
 		</main>
 	)
 }
