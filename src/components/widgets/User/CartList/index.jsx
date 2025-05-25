800f3baca855a9427c3496df0c1ba1e1364e21bb
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2025-05-25 17:43:54 +0300

    UPDATE

--- src/components/widgets/User/CartList/index.jsx
+++ src/components/widgets/User/CartList/index.jsx
@@ -1,19 +1,18 @@
 import clsx from 'clsx'
 import cls from './index.module.scss'
 import { Item } from './Item'
+import { ShoppingBasket } from 'lucide-react'
+import { UiTypography } from '@shared/ui/UiTypography'
 
 const CartList = ({ className, ...props }) => {
 	return (
 		<ul className={clsx(cls.wrapper, className)} {...props}>
-			<Item
-				data={{
-					id: 1,
-					name: 'TEST',
-					price: 241421,
-					qnt: 4,
-					type: 'Стул',
-				}}
-			/>
+			<div className={cls.empty}>
+				<ShoppingBasket />
+				<UiTypography font='Inter-R' Tag='h2'>
+					Корзина пуста
+				</UiTypography>
+			</div>
 		</ul>
 	)
 }
